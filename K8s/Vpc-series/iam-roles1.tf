cebbb364df63f188aa38242cbaee47c16764d6c6
Andreadote <andre.mj@yahoo.com>
2023-10-07 18:37:47 -0600

    e88 / Description with fix, feat (SemVer) Contributing-to: https://kennedymakhanu1.atlassian.net/browse/<JIRA-TICKET> Problem Solution Note

--- K8s/Vpc-series/iam-roles1.tf
+++ K8s/Vpc-series/iam-roles1.tf
@@ -10,15 +10,29 @@ locals {
     "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   ]
-  
+
+}
+#Role and policies for node groups
+resource "aws_iam_role" "node" {
+  name = "eks-node-group-nodes"
+  assume_role_policy = jsonencode({
+    "version" : "2012-10-17",
+    "statement" : [{
+        "Effect" : "Allow",
+        "Action" : "sts:assumeRole",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        }
+      }]
+  })
 }
 
 
 # 1. Create IAM roles for node groups
-resource "aws_iam_role" "nodes" {
-  name               = "eks-node-group-nodes"
-  assume_role_policy = data.aws_iam_policy_document.nodes.json
-}
+#resource "aws_iam_role" "nodes" {
+  #name               = "eks-node-group-nodes"
+  #assume_role_policy = data.aws_iam_policy_document.nodes.json
+#}
 
 resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
   for_each   = toset(local.node_policies)
@@ -27,10 +41,28 @@ resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
 }
 
 # 2. Create IAM role for EKS cluster
+#resource "aws_iam_role" "demo" {
+  #name               = "eks-cluster-demo"
+  #assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
+#}
+
+#Role and policies for eks cluster
 resource "aws_iam_role" "demo" {
-  name               = "eks-cluster-demo"
-  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
+  name = "eks-cluster-demo"
+  assume_role_policy = jsonencode({
+    "version" : "2012-10-17",
+    "statement" : [{
+        "Effect" : "Allow",
+        "Action" : "sts:assumeRole",
+        "Principal" : {
+          "Service" : "eks.amazonaws.com"
+        }
+      }]
+  })
 }
+
+
+
 
 resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
   for_each   = toset(local.eks_policies)
